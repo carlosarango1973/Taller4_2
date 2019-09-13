7f864ca1a782069c05ceb30463d58c1d9cbc3334
Carlos Arango Betancurt <carlos.arango.b.2019@gmail.com>
2019-09-12 21:05:01 -0500

    Desarrollo punto 2 Taller 4

---
+++ Taller 4_2/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taller_4_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = -2; i >= -20; i -= 2) {
+                listBox1.Items.Add(i);
+            }
+        }
+    }
+}
